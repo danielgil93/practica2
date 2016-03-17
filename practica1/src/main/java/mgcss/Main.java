42b298bbab073f50d1d4f362601dc094db917365
Daniel Gil Romero <daniel.gil93@gmail.com>
2016-03-17 18:17:00 +0100

    if

--- practica1/src/main/java/mgcss/Main.java
+++ practica1/src/main/java/mgcss/Main.java
@@ -7,6 +7,10 @@ package mgcss;
 public class Main {
     public static void main (String[] args){
       System.out.println("AY LMAOOOOO2222!");
+      if(2 > 3)
+        System.out.println("prueba 2");
+      else
+        System.out.println("prueba 1");
       System.out.println("Bienvenidos 1al mantenimiento");
       for (int i=0; i<10;i++){
          System.out.print(i+",");
