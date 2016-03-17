fd1ac583ae3b7f680dd5a5027b5a5c51a9ddcf4a
Daniel Gil Romero <daniel.gil93@gmail.com>
2016-03-17 18:11:32 +0100

    mod

--- practica1/src/main/java/mgcss/Main.java
+++ practica1/src/main/java/mgcss/Main.java
@@ -7,7 +7,7 @@ package mgcss;
 public class Main {
     public static void main (String[] args){
       System.out.println("AY LMAOOOOO2222!");
-      System.out.println("Bienvenidos al mantenimiento");
+      System.out.println("Bienvenidos 2al mantenimiento");
       for (int i=0; i<10;i++){
          System.out.print(i+",");
       }
