59c6f64e843a210a36f8def8b9915a1d715aa5b8
Daniel Gil Romero <daniel.gil93@gmail.com>
2016-03-17 17:58:51 +0100

    ay lmao

--- practica1/src/main/java/mgcss/Main.java
+++ practica1/src/main/java/mgcss/Main.java
@@ -6,8 +6,7 @@ package mgcss;
  */
 public class Main {
     public static void main (String[] args){
-        
-        
+      System.out.println("AY LMAOOOOO!");
       System.out.println("Bienvenidos al mantenimiento");
       for (int i=0; i<10;i++){
          System.out.print(i+",");
